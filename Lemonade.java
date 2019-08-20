5b4015685f8e01640f90a601378869fe0eae0dbe
HelenaK907 <54180186+HelenaK907@users.noreply.github.com>
2019-08-20 10:08:05 -0400

    Create Lemonade.java

---
+++ Lemonade.java
@@ -0,0 +1,17 @@
+public class Lemonade{
+
+     Scanner tasty = newScanner(System.in);
+     int cup = tasty.next.Int();
+     
+     System.out.println("Hi! How many cups do you want?");
+     
+     int cost= 2;
+     int total = cost * cup;
+     
+     if (total <= 0){;
+         System.out.println(Leave);
+     }else{
+         System.out.printf("/n You owe me $%2f", total);
+     }
+     
+}
