f7ae4fbad74e33c115b95e8c3abbee771ed24195
HelenaK907 <54180186+HelenaK907@users.noreply.github.com>
2019-08-26 19:38:33 -0400

    Update Lemonade.java

--- Lemonade.java
+++ Lemonade.java
@@ -1,17 +1,20 @@
 public class Lemonade{
 
+     public static void main(String []args){
+     }
      Scanner tasty = newScanner(System.in);
-     int cup = tasty.next.Int();
-     
+
      System.out.println("Hi! How many cups do you want?");
+     int cup = tasty.next.Int();
      
      int cost= 2;
      int total = cost * cup;
      
-     if (total <= 0){;
-         System.out.println(Leave);
+     if (total <= 0)
+         System.out.println("Leave");
      }else{
-         System.out.printf("/n You owe me $%2f", total);
+         System.out.printf("\n You owe me $%2f", total);
      }
      
+}
 }
